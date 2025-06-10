e17a6cb9d537183c992ea44fe7fc3fc594d2467c
uzair171 <jabransaleem171@gmail.com>
2025-06-10 15:58:16 +0500

    used state to render text

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/header.jsx"
+import Body from "./components/body.jsx"
+
+export default function App() {
+    
+    return (
+        <>
+            <Header/>
+            <Body />
+        </>
+    )
+}
